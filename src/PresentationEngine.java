d58bca6c3fe7f679540b3e83530f550152054fb0
chris <chrisdawson1996@gmail.com>
2018-02-08 09:23:22 +0000

    Began working on the presentation engine

---
+++ src/PresentationEngine.java
@@ -0,0 +1,3 @@
+class PresentationEngine {
+	
+}
--- src/main.java
+++ src/main.java
@@ -1,5 +1,11 @@
 
 public class main {
 	public static void main(String[] args) {
+
+		// TODO: display all types of media
+
+		// TODO: abstract that into modules I can delegate 
+
+
 	}
 }
