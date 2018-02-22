9842d8ec35713035a1cc1664d3e022f3a6a091dd
Matt <mjh585@york.ac.uk>
2018-02-22 13:27:53 +0000

    Added try-catch loop within the constructor
    Co-Authored-By: bjg513 <bjg513@users.noreply.github.com>
    Co-Authored-By: kas05 <kas05@users.noreply.github.com>

--- src/XMLParser.java
+++ src/XMLParser.java
@@ -1,14 +1,33 @@
 import org.xml.sax.*;
-//import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.DefaultHandler;
 import javax.xml.parsers.SAXParserFactory;
-import javax.xml.parsers.ParserConfigurationException
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XMLParser extends DefaultHandler{
 	String inputFile = "example.pws";
 
 	public XMLParser(String inputFile){
-		
+		try {
+			// Use the default parser
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser saxParser = factory.newSAXParser();
+			// Tell the parser to start reading the XML file
+			saxParser.parse(inputFile, this);
+		}
+		// With every try there must be catch to catch the exceptions
+		catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		} 
+		catch (SAXException saxe) {
+			saxe.printStackTrace();
+		} 
+		catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
 	}
 
 }
