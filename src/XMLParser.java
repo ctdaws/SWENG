83d6406fe8c6a8c9f5c381b02d8d47aefd22cf05
Matt <mjh585@york.ac.uk>
2018-02-22 13:24:21 +0000

    Migrated testing of parser from main to XMLParser
    Co-Authored-By: bjg513 <bjg513@users.noreply.github.com>
    Co-Authored-By: kas05 <kas05@users.noreply.github.com>

---
+++ src/XMLParser.java
@@ -0,0 +1,14 @@
+import org.xml.sax.*;
+//import org.xml.sax.helpers.DefaultHandler;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.ParserConfigurationException
+import javax.xml.parsers.SAXParser;
+
+public class XMLParser extends DefaultHandler{
+	String inputFile = "example.pws";
+
+	public XMLParser(String inputFile){
+		
+	}
+
+}
--- src/main.java
+++ src/main.java
@@ -1,12 +1,7 @@
-import org.xml.sax.*;
-//import org.xml.sax.helpers.DefaultHandler;
-import javax.xml.parsers.SAXParserFactory;
-import javax.xml.parsers.ParserConfigurationException
-import javax.xml.parsers.SAXParser;
-
 
 public class Main {
-	public static void main(String[] args) {		
+	public static void main(String[] args){		
+		
 
 		
 	}
