dce2ceef1d3024fe058579f97fc2c10649cdcf52
Matt <mjh585@york.ac.uk>
2018-02-22 13:17:06 +0000

    Added imports for XML parsing
    Co-Authored-By: bjg513 <bjg513@users.noreply.github.com>
    Co-Authored-By: kas05 <kas05@users.noreply.github.com>

--- src/main.java
+++ src/main.java
@@ -1,11 +1,13 @@
+import org.xml.sax.*;
+//import org.xml.sax.helpers.DefaultHandler;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.ParserConfigurationException
+import javax.xml.parsers.SAXParser;
+
+
 public class Main {
 	public static void main(String[] args) {		
 
-		Image testImage = new Image("../resources/sampleImg.jpg", new Position(0.0f, 0.0f), 0);
-
-		Text testText = new Text("Sample", new Position(0.0f, 0.0f), 0);
-
-		Audio testAudio = new Audio("../resources/sampleAudio.wav");
-		testAudio.play();				//play audio
+		
 	}
 }
