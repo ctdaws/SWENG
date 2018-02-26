c89af494e52df1d626559330022b9555e24b6158
ojt507 <36478777+ojt507@users.noreply.github.com>
2018-02-26 14:23:35 +0000

    Changed Slide class

--- src/Slide.java
+++ src/Slide.java
@@ -1,15 +1,10 @@
 import javafx.scene.text.Text;
 
 class Slide {
-	// TEST: some sample text;
-	public Text text;
 
-	public String ID;
+	public String id;
 
-	public Slide(String ID) {
-		this.ID = ID;
-		text = new Text("Slide:" + ID);
-		text.setLayoutX(50);
-		text.setLayoutY(50);
+	public Slide(String id) {
+		this.id = id;
 	} 
 }
