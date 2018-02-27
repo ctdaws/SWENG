ed7b9db7a15ae7e6fe60a5f8c46817c095edfcf4
SB800046 <36273998+SB800046@users.noreply.github.com>
2018-02-27 15:22:02 +0000

    Reduced code in FLAudio

--- src/FLAudio.java
+++ src/FLAudio.java
@@ -1,38 +1,14 @@
-// NOTE (Chris): The system so far will NOT play any mp3 files
-// NOTE (Samuel): javafx MediaPlayer CAN play mp3 files
-
 import java.io.File;
-//import javax.sound.sampled.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class FLAudio {
 
 	private Position position;
-
-//	private Clip audioClip;
-
-	private Media media;
 	private MediaPlayer mediaPlayer;
 
-	// public FLAudio(String audioPath, Position position) {
-	// 	try {
-	// 		AudioInputStream stream = AudioSystem.getAudioInputStream(new File(audioPath).getAbsoluteFile());
-	//         this.audioClip = AudioSystem.getClip();
-	//         this.audioClip.open(stream);
-	//         this.position = position;
-	// 	} catch (Exception e) {
-	// 		e.printStackTrace();
-	// 	}
-	// }
-	//
-	// public void play() {
-	// 	this.audioClip.start();
-	// }
-
 	public FLAudio(String audioPath, Position position) {
-			this.media = new Media(new File(audioPath).toURI().toString());
-			this.mediaPlayer = new MediaPlayer(this.media);
+			this.mediaPlayer = new MediaPlayer(new Media(new File(audioPath).toURI().toString()));
 			this.position = position;
 	}
 
