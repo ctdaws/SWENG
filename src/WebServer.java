bdaece5f26670872c9c8d5d64f310723a963bb68
SB800046 <36273998+SB800046@users.noreply.github.com>
2018-05-08 13:35:58 +0100

    added sample html

--- src/WebServer.java
+++ src/WebServer.java
@@ -34,7 +34,7 @@ public class WebServer {
         @Override
 
         public void handle(HttpExchange he) throws IOException {
-            String response = "<html><h1>Server start success if you see this message</h1>" + "<h1>Port: " + port + "</h1></html>";
+            String response = this.getClass().getResource("html_test.html").toString();
             he.sendResponseHeaders(200, response.length());
             OutputStream os = he.getResponseBody();
             os.write(response.getBytes());
