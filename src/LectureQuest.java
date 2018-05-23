bc612b595d9f574590cda5cf3c830997d4baca2f
Ben Grainger <bengrainger@bens-mbp-6.home>
2018-05-23 18:12:53 +0100

    Refined LectureQuest Class
    Solved all errors
    TODO Make it actually create the GUI ;)

--- src/LectureQuest.java
+++ src/LectureQuest.java
@@ -48,7 +48,7 @@ public class LectureQuest extends Application {
     //TODO Make sure this does what it's supposed to
     primaryStage.getIcons().add(new Image("file:../resources/LQ_logo_2_32.png"));
 
-    createAndShowGUI(primaryStage);
+    
 
     File questXml = openFile(primaryStage);
 
@@ -94,6 +94,115 @@ public class LectureQuest extends Application {
         }
       });
 
+      //createAndShowGUI(primaryStage, presentation);
+      Label presentationLabel = new Label("Slide: ");// + Integer.toString(slideNum));
+
+    //createContentPane(primaryStage); 
+    //createBottomPane(primaryStage, presentation);
+
+      //TODO Refactor into a new method
+      //create Menu Pane
+    {
+      MenuBar menuBar = new MenuBar();
+      Menu levels = new Menu("Levels");
+      ArrayList<Menu> levelItems = new ArrayList<Menu>(); //Levels array
+      ArrayList<ArrayList<MenuItem>> levelQuestions = new ArrayList<ArrayList<MenuItem>>(); //Array of the questions array for each level
+
+      //this.presentationLabel = new Label("Level: " + Integer.toString(levelNum) + " Question: " + Integer.toString(qNum));
+
+      //TODO Sort this out fot getting number of levels and questions
+      for(i=0; i<5; i++) {
+        levelItems.add(new Menu("Level " + (i+1)));
+        ArrayList<MenuItem> questions = new ArrayList<MenuItem>();  //Array of questions in current level.
+        for(j=0; j<5; j++) {
+          questions.add(new MenuItem("Question: " + (j+1)));
+          questions.get(j).setId(i+"/"+j);
+          levelItems.get(i).getItems().add(questions.get(j));
+          questions.get(j).setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+              Object o = event.getSource();
+              for (int k=0; k<questions.size(); k++) {
+                if(o == questions.get(k)) {
+
+                  String menuID = questions.get(k).getId();
+                  String menuIDArray[] = menuID.split("/");
+                  int levelNum = Integer.parseInt(menuIDArray[0]);
+                  int questionNum = Integer.parseInt(menuIDArray[1]);
+                  setSlide(levelNum, questionNum);
+                  //
+                }
+              }
+            }
+          });
+        }
+        levels.getItems().add(levelItems.get(i));
+        levelQuestions.add(questions);
+      }
+      menuBar.getMenus().add(levels);
+      //VBox vBox = new VBox(menuBar);
+
+      VBox vBox = new VBox(menuBar);
+      BorderPane borderLayout = new BorderPane();
+      borderLayout.setTop(vBox);
+      // borderLayout.setCenter(presentationLabel);
+    }
+
+
+      //TODO Refactor into a new method
+      //create Bottom Pane
+     {
+
+        Button nextBtn = new Button("Next");
+        Button prevBtn = new Button("Previous");
+        prevBtn.setDisable(true);
+
+        nextBtn.setOnAction(new EventHandler<ActionEvent>() {
+          @Override
+          public void handle(ActionEvent event) {
+            presentation.moveNextSlide();
+            prevBtn.setDisable(false);
+          }
+        });
+
+        prevBtn.setOnAction(new EventHandler<ActionEvent>() {
+          @Override
+          public void handle(ActionEvent event) {
+            presentation.moveBackSlide();
+            if(0 == 1) {//TODO if MENU
+              prevBtn.setDisable(true);
+            }
+          }
+        });
+
+        StackPane root = new StackPane();
+       
+        HBox menu = new HBox();
+        menu.setSpacing(10);
+        menu.setMargin(prevBtn, new Insets(20, 20, 20, 20));
+        menu.setMargin(nextBtn, new Insets(20, 20, 20, 20));
+
+        
+
+        BorderPane borderLayout = new BorderPane();
+
+        //borderLayout.setTop(levelMenu.showMenu());
+        //borderLayout.setTop(vBox);
+        borderLayout.setCenter(presentationLabel);
+        borderLayout.setBottom(menu);
+
+        root.getChildren().add(borderLayout);
+
+        menu.setBackground(new Background(new BackgroundFill(Color.web("#FF0000"), CornerRadii.EMPTY, Insets.EMPTY)));
+        //presentationLabel.setBackground(new Background(new BackgroundFill(Color.web("#FFFF00"), CornerRadii.EMPTY, Insets.EMPTY)));
+
+        menu.getChildren().add(prevBtn);
+        menu.getChildren().add(nextBtn);
+     }
+
+
+
+
       primaryStage.setScene(scene);
       primaryStage.show();
     }
@@ -119,7 +228,7 @@ public class LectureQuest extends Application {
   public void setSlide(int newLevel, int newQuestion) {
     this.levelNum = newLevel + 1;
     this.qNum = (newQuestion + 1);
-    this.presentationLabel.setText("Level: " + Integer.toString(levelNum) + " Question: " + Integer.toString(qNum));
+    //this.presentationLabel.setText("Level: " + Integer.toString(levelNum) + " Question: " + Integer.toString(qNum));
     System.out.println("Level: " + Integer.toString(levelNum) + " Question: " + Integer.toString(qNum));
     this.presentation.moveSlide(CombineMenuID(levelNum, qNum));
   }
@@ -129,112 +238,209 @@ public class LectureQuest extends Application {
     return newMenuID;
   }
 
-  public void createContentPane(Stage primaryStage) {
-    MenuBar menuBar = new MenuBar();
-    Menu levels = new Menu("Levels");
-    ArrayList<Menu> levelItems = new ArrayList<Menu>(); //Levels array
-    ArrayList<ArrayList<MenuItem>> levelQuestions = new ArrayList<ArrayList<MenuItem>>(); //Array of the questions array for each level
+  // public void createContentPane(Stage primaryStage) {
+  //   {
+  //     MenuBar menuBar = new MenuBar();
+  //     Menu levels = new Menu("Levels");
+  //     ArrayList<Menu> levelItems = new ArrayList<Menu>(); //Levels array
+  //     ArrayList<ArrayList<MenuItem>> levelQuestions = new ArrayList<ArrayList<MenuItem>>(); //Array of the questions array for each level
 
-    this.presentationLabel = new Label("Level: " + Integer.toString(levelNum) + " Question: " + Integer.toString(qNum));
+  //     this.presentationLabel = new Label("Level: " + Integer.toString(levelNum) + " Question: " + Integer.toString(qNum));
 
-    //TODO Sort this out fot getting number of levels and questions
-    for(i=0; i<5; i++) {
-      levelItems.add(new Menu("Level " + (i+1)));
-      ArrayList<MenuItem> questions = new ArrayList<MenuItem>();  //Array of questions in current level.
-      for(j=0; j<5; j++) {
-        questions.add(new MenuItem("Question: " + (j+1)));
-        questions.get(j).setId(i+"/"+j);
-        levelItems.get(i).getItems().add(questions.get(j));
-        questions.get(j).setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-            Object o = event.getSource();
-            for (int k=0; k<questions.size(); k++) {
-              if(o == questions.get(k)) {
+  //     //TODO Sort this out fot getting number of levels and questions
+  //     for(i=0; i<5; i++) {
+  //       levelItems.add(new Menu("Level " + (i+1)));
+  //       ArrayList<MenuItem> questions = new ArrayList<MenuItem>();  //Array of questions in current level.
+  //       for(j=0; j<5; j++) {
+  //         questions.add(new MenuItem("Question: " + (j+1)));
+  //         questions.get(j).setId(i+"/"+j);
+  //         levelItems.get(i).getItems().add(questions.get(j));
+  //         questions.get(j).setOnAction(new EventHandler<ActionEvent>() {
+  //           @Override
+  //           public void handle(ActionEvent event) {
+  //             Object o = event.getSource();
+  //             for (int k=0; k<questions.size(); k++) {
+  //               if(o == questions.get(k)) {
 
-                String menuID = questions.get(k).getId();
-                String menuIDArray[] = menuID.split("/");
-                int levelNum = Integer.parseInt(menuIDArray[0]);
-                int questionNum = Integer.parseInt(menuIDArray[1]);
-                setSlide(levelNum, questionNum);
-                //
-              }
-            }
-          }
-        });
-      }
-      levels.getItems().add(levelItems.get(i));
-      levelQuestions.add(questions);
-    }
-    menuBar.getMenus().add(levels);
+  //                 String menuID = questions.get(k).getId();
+  //                 String menuIDArray[] = menuID.split("/");
+  //                 int levelNum = Integer.parseInt(menuIDArray[0]);
+  //                 int questionNum = Integer.parseInt(menuIDArray[1]);
+  //                 setSlide(levelNum, questionNum);
+  //                 //
+  //               }
+  //             }
+  //           }
+  //         });
+  //       }
+  //       levels.getItems().add(levelItems.get(i));
+  //       levelQuestions.add(questions);
+  //     }
+  //     menuBar.getMenus().add(levels);
 
-    VBox vBox = new VBox(menuBar);
-    BorderPane borderLayout = new BorderPane();
-    borderLayout.setTop(vBox);
-    borderLayout.setCenter(presentationLabel);
-  }
+  //     VBox vBox = new VBox(menuBar);
+  //     BorderPane borderLayout = new BorderPane();
+  //     borderLayout.setTop(vBox);
+  //     borderLayout.setCenter(presentationLabel);
+  //   }
+  // }
 
-  public void createBottomPane(Stage primaryStage) {
-    int slideNum = 1;
-    Label presentation = new Label("Slide: " + Integer.toString(slideNum));
-    System.out.println("Slide: " + Integer.toString(slideNum));
+  
+  // public void createAndShowGUI(Stage primaryStage, Presentation presentation) { 
+  //    // Label presentationLabel = new Label("Slide: ");// + Integer.toString(slideNum));
 
-    Button nextBtn = new Button("Next");
-    Button prevBtn = new Button("Previous");
-    prevBtn.setDisable(true);
+  //   //createContentPane(primaryStage); 
+  //   //createBottomPane(primaryStage, presentation);
 
-    nextBtn.setOnAction(new EventHandler<ActionEvent>() {
-      @Override
-      public void handle(ActionEvent event) {
-        nextSlideNum();
-        presentation.setText("Slide: " + Integer.toString(slideNum));
-        System.out.println("Slide: " + Integer.toString(slideNum));
-        prevBtn.setDisable(false);
-      }
-    });
-    prevBtn.setOnAction(new EventHandler<ActionEvent>() {
-      @Override
-      public void handle(ActionEvent event) {
-        prevSlideNum();
-        presentation.setText("Slide: " + Integer.toString(slideNum));
-        System.out.println("Slide: " + Integer.toString(slideNum));
-        if(slideNum == 1) {
-          prevBtn.setDisable(true);
-        }
-      }
-    });
+  //     //TODO Refactor into a new method
+  //     //create Menu Pane
+  //   {
+  //     MenuBar menuBar = new MenuBar();
+  //     Menu levels = new Menu("Levels");
+  //     ArrayList<Menu> levelItems = new ArrayList<Menu>(); //Levels array
+  //     ArrayList<ArrayList<MenuItem>> levelQuestions = new ArrayList<ArrayList<MenuItem>>(); //Array of the questions array for each level
 
-    StackPane root = new StackPane();
-    //Pane presentation = new Pane();
-    //Pane menu = new Pane();
-    HBox menu = new HBox();
-    menu.setSpacing(10);
-    menu.setMargin(prevBtn, new Insets(20, 20, 20, 20));
-    menu.setMargin(nextBtn, new Insets(20, 20, 20, 20));
-    BorderPane borderLayout = new BorderPane();
+  //     //this.presentationLabel = new Label("Level: " + Integer.toString(levelNum) + " Question: " + Integer.toString(qNum));
 
-    //LevelMenu levelMenu = new LevelMenu();
-    //  borderLayout.setTop(levelMenu.showMenu());
-    
-    borderLayout.setCenter(presentation);
-    borderLayout.setBottom(menu);
-    //BorderPane borderLayout = new BorderPane();
-    // borderLayout.setCenter(presentation);
-    // borderLayout.setBottom(menu);
+  //     //TODO Sort this out fot getting number of levels and questions
+  //     for(i=0; i<5; i++) {
+  //       levelItems.add(new Menu("Level " + (i+1)));
+  //       ArrayList<MenuItem> questions = new ArrayList<MenuItem>();  //Array of questions in current level.
+  //       for(j=0; j<5; j++) {
+  //         questions.add(new MenuItem("Question: " + (j+1)));
+  //         questions.get(j).setId(i+"/"+j);
+  //         levelItems.get(i).getItems().add(questions.get(j));
+  //         questions.get(j).setOnAction(new EventHandler<ActionEvent>() {
+  //           @Override
+  //           public void handle(ActionEvent event) {
+  //             Object o = event.getSource();
+  //             for (int k=0; k<questions.size(); k++) {
+  //               if(o == questions.get(k)) {
 
-    root.getChildren().add(borderLayout);
+  //                 String menuID = questions.get(k).getId();
+  //                 String menuIDArray[] = menuID.split("/");
+  //                 int levelNum = Integer.parseInt(menuIDArray[0]);
+  //                 int questionNum = Integer.parseInt(menuIDArray[1]);
+  //                 setSlide(levelNum, questionNum);
+  //                 //
+  //               }
+  //             }
+  //           }
+  //         });
+  //       }
+  //       levels.getItems().add(levelItems.get(i));
+  //       levelQuestions.add(questions);
+  //     }
+  //     menuBar.getMenus().add(levels);
 
-    menu.setBackground(new Background(new BackgroundFill(Color.web("#FF0000"), CornerRadii.EMPTY, Insets.EMPTY)));
-    presentation.setBackground(new Background(new BackgroundFill(Color.web("#FFFF00"), CornerRadii.EMPTY, Insets.EMPTY)));
+  //     // VBox vBox = new VBox(menuBar);
+  //     // BorderPane borderLayout = new BorderPane();
+  //     // borderLayout.setTop(vBox);
+  //     // borderLayout.setCenter(presentationLabel);
+  //   }
 
-    menu.getChildren().add(prevBtn);
-    menu.getChildren().add(nextBtn);
-  }
 
-  public void createAndShowGUI(Stage primaryStage) { 
-    createContentPane(primaryStage); 
-    createBottomPane(primaryStage);
-  }
+  //     //TODO Refactor into a new method
+  //     //create Bottom Pane
+  //   {
+
+  //       Button nextBtn = new Button("Next");
+  //       Button prevBtn = new Button("Previous");
+  //       prevBtn.setDisable(true);
+
+  //       nextBtn.setOnAction(new EventHandler<ActionEvent>() {
+  //         @Override
+  //         public void handle(ActionEvent event) {
+  //           this.presentation.moveNextSlide();
+  //           prevBtn.setDisable(false);
+  //         }
+  //       });
+
+  //       prevBtn.setOnAction(new EventHandler<ActionEvent>() {
+  //         @Override
+  //         public void handle(ActionEvent event) {
+  //           this.presentation.moveBackSlide();
+  //           if(0 == 1) {//TODO if MENU
+  //             prevBtn.setDisable(true);
+  //           }
+  //         }
+  //       });
+
+  //       StackPane root = new StackPane();
+       
+  //       HBox menu = new HBox();
+  //       menu.setSpacing(10);
+  //       menu.setMargin(prevBtn, new Insets(20, 20, 20, 20));
+  //       menu.setMargin(nextBtn, new Insets(20, 20, 20, 20));
+
+  //       VBox vBox = new VBox(menuBar);
+      
+
+  //       BorderPane borderLayout = new BorderPane();
+
+  //       //borderLayout.setTop(levelMenu.showMenu());
+  //       borderLayout.setTop(vBox);
+  //       borderLayout.setCenter(presentation);
+  //       borderLayout.setBottom(menu);
+
+  //       root.getChildren().add(borderLayout);
+
+  //       menu.setBackground(new Background(new BackgroundFill(Color.web("#FF0000"), CornerRadii.EMPTY, Insets.EMPTY)));
+  //       //presentationLabel.setBackground(new Background(new BackgroundFill(Color.web("#FFFF00"), CornerRadii.EMPTY, Insets.EMPTY)));
+
+  //       menu.getChildren().add(prevBtn);
+  //       menu.getChildren().add(nextBtn);
+  //   }
+
+  // }
+
+  // public void createBottomPane(Stage primaryStage, Presentation presentation) {
+
+  //   Label presentationLabel = new Label("Slide: ");// + Integer.toString(slideNum));
+
+  //   Button nextBtn = new Button("Next");
+  //   Button prevBtn = new Button("Previous");
+  //   prevBtn.setDisable(true);
+
+  //   nextBtn.setOnAction(new EventHandler<ActionEvent>() {
+  //     @Override
+  //     public void handle(ActionEvent event) {
+  //       this.presentation.moveNextSlide();
+  //       prevBtn.setDisable(false);
+  //     }
+  //   });
+
+  //   prevBtn.setOnAction(new EventHandler<ActionEvent>() {
+  //     @Override
+  //     public void handle(ActionEvent event) {
+  //       this.presentation.moveBackSlide();
+  //       if(0 == 1) {//TODO if MENU
+  //         prevBtn.setDisable(true);
+  //       }
+  //     }
+  //   });
+
+  //   StackPane root = new StackPane();
+   
+  //   HBox menu = new HBox();
+  //   menu.setSpacing(10);
+  //   menu.setMargin(prevBtn, new Insets(20, 20, 20, 20));
+  //   menu.setMargin(nextBtn, new Insets(20, 20, 20, 20));
+
+  //   BorderPane borderLayout = new BorderPane();
+
+  //   borderLayout.setTop(levelMenu.showMenu());
+  //   borderLayout.setCenter(presentation);
+  //   borderLayout.setBottom(menu);
+
+  //   root.getChildren().add(borderLayout);
+
+  //   menu.setBackground(new Background(new BackgroundFill(Color.web("#FF0000"), CornerRadii.EMPTY, Insets.EMPTY)));
+  //   presentationLabel.setBackground(new Background(new BackgroundFill(Color.web("#FFFF00"), CornerRadii.EMPTY, Insets.EMPTY)));
+
+  //   menu.getChildren().add(prevBtn);
+  //   menu.getChildren().add(nextBtn);
+  // }
 
   @Override
   public void stop() { Platform.exit(); }
