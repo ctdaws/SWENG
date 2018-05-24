4e9ac3e57892e09fbf0c8e1164e7802e583f2fed
chris <chrisdawson1996@gmail.com>
2018-05-24 16:30:22 +0100

    Getting a string from EchoResponse

--- src/CommsTest.java
+++ src/CommsTest.java
@@ -1,14 +1,59 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.Socket;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class CommsTest {
 
     public static void main(String args[]) {
+
+        CommsTest commsTest = new CommsTest();
+
+        String url = "http://lecturequest.york.ac.uk/echoResponse";
+
         try {
-            Socket socket = new Socket("144.32.123.31", 11427);
-            System.out.println("Connection");
-        } catch(IOException e){
+            commsTest.sendGet(url);
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+    // HTTP GET request
+    private void sendGet(String url) throws Exception {
+
+        String USER_AGENT = "Mozilla/5.0";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
     }
 }
