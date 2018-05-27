077f7a1b46d25d0e612d4080b5aa11393da2b743
SB800046 <36273998+SB800046@users.noreply.github.com>
2018-05-27 17:58:49 +0100

    Modified Launcher?

--- src/LandingPage.java
+++ src/LandingPage.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
